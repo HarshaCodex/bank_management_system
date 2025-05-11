bc8eab22451b17a5f20863c9af42f30cec4cbb36
HarshaCodex <harshacodex@gmail.com>
2025-05-11 16:17:46 -0500

    Add @property method

--- bank_management_system/accounts.py
+++ bank_management_system/accounts.py
@@ -12,11 +12,12 @@ class Account:
     
     def withdraw(self, amount):
         if amount < 0:
-            raise ValueError("Amount must be positve")
+            raise ValueError("Amount must be positive")
         if self.__balance < amount:
             raise ValueError("Insufficient funds")
         self.__balance -= amount
         return self.__balance
     
-    def get_balance(self):
+    @property
+    def balance(self):
         return self.__balance
