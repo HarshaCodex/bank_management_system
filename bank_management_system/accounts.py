7ed4c44dbef00443a418992e0d7e95ada7ac3765
HarshaCodex <harshacodex@gmail.com>
2025-05-11 16:40:27 -0500

    Add transaction_log and transaction_history

--- bank_management_system/accounts.py
+++ bank_management_system/accounts.py
@@ -3,11 +3,14 @@ class Account:
         self.account_number = account_number
         self.account_holder = account_holder
         self.__balance = balance
+        self.transaction_history = []
     
     def deposit(self, amount):
         if amount < 0:
             raise ValueError("Amount must be positive")
         self.__balance += amount
+        transaction_log = {"type": "Deposit", "amount": amount, "balance": self.balance}
+        self.transaction_history.append(transaction_log)
         return self.__balance
     
     def withdraw(self, amount):
@@ -16,8 +19,13 @@ class Account:
         if self.__balance < amount:
             raise ValueError("Insufficient funds")
         self.__balance -= amount
+        transaction_log = {"type": "Withdraw", "amount": amount, "balance": self.balance}
+        self.transaction_history.append(transaction_log)
         return self.__balance
     
     @property
     def balance(self):
         return self.__balance
+    
+    def get_transaction_history(self):
+        return self.transaction_history
