a5978f54939110f3922a05b3ea9bf9e0ca181f93
HarshaCodex <harshacodex@gmail.com>
2025-05-12 20:32:33 -0500

    Implement exit option

--- bank_management_system/main.py
+++ bank_management_system/main.py
@@ -1,6 +1,7 @@
 from bank import Bank
 
 def main():
+    bank = Bank()
     while True:
         print("\n===== Bank Menu =====")
         print("1. Create Account")
@@ -10,7 +11,11 @@ def main():
         print("5. View Account")
         print("6. View All Accounts")
         print("7. Exit")
-        break
+        
+        user_choice = int(input("Please enter your choice(1-7):"))
+
+        if user_choice == 7:
+            break
 
 if __name__ == "__main__":
     main()
