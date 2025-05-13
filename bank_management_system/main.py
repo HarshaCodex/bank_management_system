4f82838f827dbac8fb0fe99cfd0ae9fee6d43e48
HarshaCodex <harshacodex@gmail.com>
2025-05-12 20:27:33 -0500

    Create CLI menu for user to interact

---
+++ bank_management_system/main.py
@@ -0,0 +1,16 @@
+from bank import Bank
+
+def main():
+    while True:
+        print("\n===== Bank Menu =====")
+        print("1. Create Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Transfer")
+        print("5. View Account")
+        print("6. View All Accounts")
+        print("7. Exit")
+        break
+
+if __name__ == "__main__":
+    main()
